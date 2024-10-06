4ed4eedc47a2829379e8ad31f39b25e0688e7802
2021471 <2021471@INLT3580>
2024-10-06 23:38:39 +0530

    XML comments & Unit testing completed

---
+++ BankingProjectTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankingProjectTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("QuEST-Global")]
+[assembly: AssemblyProduct("BankingProjectTests")]
+[assembly: AssemblyCopyright("Copyright © QuEST-Global 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5d93760-0b19-441a-8316-4a1276cb846d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
